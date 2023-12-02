d4c139a3186e3889937549aa0b93dd84013f3184
Nuria <nuriavc65@educastur.es>
2023-12-02 17:46:11 +0100

    lloracion

--- src/Unidad3/ServidorErmetico/Unidad3Ejercicio4.java
+++ src/Unidad3/ServidorErmetico/Unidad3Ejercicio4.java
@@ -1,8 +1,21 @@
 package Unidad3.ServidorErmetico;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Unidad3Ejercicio4 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		ServerSocket server = new ServerSocket();
+		Socket socket = server.accept();
+		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+		DataInputStream in = new DataInputStream(socket.getInputStream());
+		while(true) {
+			
+		}
 		
 	}
 
